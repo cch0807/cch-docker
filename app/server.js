401594e3b6611946caec53b5598694ab31f4900d
cch0807 <ch.choi@aizen.co>
2022-11-18 18:59:45 +0900

    20221118_1

---
+++ app/server.js
@@ -0,0 +1,15 @@
+// Express 모듈 불러오기
+const express = require('express');
+
+// Constants
+const PORT = 8080; // Express 서버를 위한 포트 설정
+const HOST = '0.0.0.0'; // 호스트 지정
+
+// App
+const app = express(); // 새로운 Express 어플 생성
+app.get('/', (req, res) => { 
+    res.send('Hello Wolrd');
+}); // "/" 경로로 요청이 오면 결과값으로 반환
+
+app.listen(PORT, HOST); // 해당 포트와 호스트에서 HTTP 서버를 시작 
+console.log('Running on http://${HOST}:${PORT}');
